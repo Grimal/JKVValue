4be197acdfc2d03414c8b63b439c3bbc1f76096e
Brian Grimal <bgrimal@tifco.com>
2023-02-05 10:47:57 -0600

    Set paths

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JKVValue",
-            dependencies: []),
+            dependencies: [],
+            path: "JKVValue",
+            publicHeadersPath: "."
+        ),
         .testTarget(
             name: "JKVValueTests",
             dependencies: ["JKVValue"]),
